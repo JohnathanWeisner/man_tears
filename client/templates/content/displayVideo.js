f058c5c704cf925aae031bf06086f366c395aaef
blooies <joy28@msn.com>
2016-09-11 14:18:58 -0400

    deleted unused files

--- client/templates/content/displayVideo.js
+++
@@ -1,7 +0,0 @@
-Template.displayVideo.helpers({
-    formatUrl: function(urlId) {
-        var formattedUrl = "https://www.youtube.com/embed/"
-        formattedUrl = formattedUrl + urlId;
-        return formattedUrl;
-    }
-})
