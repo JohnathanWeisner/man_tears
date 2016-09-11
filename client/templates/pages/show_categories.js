a88439695f31cd8d3844155c7580072bd220aa9b
blooies <joy28@msn.com>
2016-09-11 10:27:53 -0400

    updated show categories page

--- client/templates/pages/show_categories.js
+++ client/templates/pages/show_categories.js
@@ -1,5 +1,15 @@
 Template.showCategories.helpers({
     categories: function() {
         return Categories.find({});
+    },
+    videos: function() {
+        console.log(this)
+        var a = Content.find({categoryId: this._id});
+        console.log(a);
+        return a;
+    },
+    grabThumbnail: function() {
+        var thumbnailUrl = "http://img.youtube.com/vi/" + this.videoId + "/mqdefault.jpg";
+        return thumbnailUrl;
     }
 })
