d7bd930750706e8b3c2e882ec183f11669cc99b9
Johnathan Weisner <joweisner@gmail.com>
2016-09-10 23:00:25 -0400

    Added in basic BS for messages to use on goals page

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,43 @@
 if (Goals.find().count() === 0) {
     Goals.insert({
         text: 'Like a 16 year old girl',
-        description: '6 - 8 cry sessions a month'
+        description: '6 - 8 cry sessions a month',
+        minutes: 120,
+        messages: [
+            {percent: 0, text: 'Do I have to beat the tears out of you?'},
+            {percent: 1, text: 'Like a 16 year old girl: 1 percent'},
+            {percent: 25, text: 'Like a 16 year old girl: 25 percent'},
+            {percent: 50, text: 'Like a 16 year old girl: 50 percent'},
+            {percent: 75, text: 'Like a 16 year old girl: 75 percent'},
+            {percent: 100, text: 'Like a 16 year old girl: 100 percent'}
+        ]
     });
 
     Goals.insert({
         text: 'Above and beyond',
-        description: '4 - 6 sessions a month'
+        description: '4 - 6 sessions a month',
+        minutes: 60,
+        messages: [
+            {percent: 0, text: 'You aint dun shit.'},
+            {percent: 1, text: 'Above and beyond: 1 percent'},
+            {percent: 25, text: 'Above and beyond: 25 percent'},
+            {percent: 50, text: 'Above and beyond: 50 percent'},
+            {percent: 75, text: 'Above and beyond: 75 percent'},
+            {percent: 100, text: 'Above and beyond: 100 percent'}
+        ]
     })
 
     Goals.insert({
         text: 'Feminist',
-        description: '3 cry sessions a month'
+        description: '3 cry sessions a month',
+        minutes: 20,
+        messages: [
+            {percent: 0, text: 'Bitch please.'},
+            {percent: 1, text: 'Feminist: 1 percent'},
+            {percent: 25, text: 'Feminist: 25 percent'},
+            {percent: 50, text: 'Feminist: 50 percent'},
+            {percent: 75, text: 'Feminist: 75 percent'},
+            {percent: 100, text: 'Feminist: 100 percent'}
+        ]
     })
 }
