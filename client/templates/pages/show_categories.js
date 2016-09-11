1466213ec3ab6297326ba84b44aac52a64f823ec
blooies <joy28@msn.com>
2016-09-11 12:56:55 -0400

    added youtube overlay

--- client/templates/pages/show_categories.js
+++ client/templates/pages/show_categories.js
@@ -13,7 +13,44 @@ Template.showCategories.helpers({
     videoInfo: function() {
         var self = this;
         Meteor.call('getVideoInfo', this.videoId, function(error, response) {
-            document.getElementById(self.videoId).innerHTML = response.title;
+            document.getElementById('title-'+self.videoId).innerHTML = response.title;
         })
+    }
+})
+
+Template.showCategories.events({
+    'click .thumbnail': function(a, b, c) {
+        var video = document.getElementById("youtube-video");
+        console.log(video)
+        if (video && video.src) {
+            var frame = document.getElementById("youtube-video");
+            frame.parentNode.removeChild(frame);
+            $('.overlay').append("<div id='youtube-video></div>");
+            console.log("appended!!!")
+        }
+        var videoId = this.videoId;
+        var player;
+        function onYouTubeIframeAPIReady() {
+            console.log("HERE", videoId)
+            player = new YT.Player('youtube-video', {
+              height: '390',
+              width: '640',
+              videoId: videoId,
+              events: {
+                 'onReady': onPlayerReady,
+              }
+            });
+        }
+
+        function onPlayerReady(event) {
+            event.target.playVideo();
+            // var iframe = document.getElementById('2IPJEnOT9AQ');
+            // var requestFullScreen = iframe.requestFullScreen || iframe.mozRequestFullScreen || iframe.webkitRequestFullScreen;
+            // if (requestFullScreen) {
+            //     requestFullScreen.bind(iframe)();
+            // }
+        }
+
+        onYouTubeIframeAPIReady();
     }
 })
